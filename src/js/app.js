7e0625a420f48e3c2bd3307132949e7414f5d7af
Débora <deboradiazper@gmail.com>
2022-06-07 18:35:33 +0000

    se arreglan ternarias

--- src/js/app.js
+++ src/js/app.js
@@ -41,16 +41,16 @@ function render(variables = {}) {
   }</h3>
           <ul class=${variables.socialMediaPosition}>
             <li><a href="https://twitter.com/${
-              variables.twitter ? variables.socialMediaPosition : " "
+              variables.twitter ? variables.twitter : " "
             }"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/${
-              variables.github ? variables.socialMediaPosition : " "
+              variables.github ? variables.github : " "
             }"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/${
-              variables.linkedin ? variables.socialMediaPosition : " "
+              variables.linkedin ? variables.linkedin : " "
             }"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="https://instagram.com/${
-              variables.instagram ? variables.socialMediaPosition : " "
+              variables.instagram ? variables.instagram : " "
             }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
